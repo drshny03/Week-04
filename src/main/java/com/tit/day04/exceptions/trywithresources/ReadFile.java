9824dd192152c5f4231fceb5998592acf375e272
drshny03 <darshanyadav7049@gmail.com>
2025-02-14 13:32:37 +0530

    [Darshan Yadav] Add . try-with-resources (Auto-closing Resources)

---
+++ src/main/java/com/tit/day04/exceptions/trywithresources/ReadFile.java
@@ -0,0 +1,22 @@
+package com.tit.day04.exceptions.trywithresources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        String fileName = "Source.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String firstLine = reader.readLine();
+            if (firstLine != null) {
+                System.out.println(firstLine);
+            } else {
+                System.out.println("File is empty");
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
